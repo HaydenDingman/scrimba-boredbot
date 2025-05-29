5fcdcb2545c897b8b12bacc69ae7cb3ac3692cc7
Hayden Dingman <haydendingman@gmail.com>
2025-05-29 13:00:39 -0700

    Fix bug with button hookup -> class to id

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ document.querySelector("button").addEventListener("click", () => {
   fetch("https://apis.scrimba.com/bored/api/activity")
   .then(response => response.json())
   .then(data => {
-    document.getElementById("activity-container").innerText = data.activity;
+    document.getElementById("activity-container").textContent = data.activity;
   })
 })
